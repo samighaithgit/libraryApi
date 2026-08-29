3eb238cd840fd049095b962c7f90f18a9115a15b
Saba Amjad <sayyash02@gmail.com>
2026-08-30 00:39:30 +0300

    feat: add auth guard and move JWT secrets to env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 import { UsersModule } from '../users/users.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
 
 @Module({
-
   imports: [
-
     UsersModule,
 
-    JwtModule.register({
-      secret: 'access-secret-key',
-    }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
 
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>(
+          'JWT_ACCESS_SECRET',
+        ),
+      }),
+    }),
   ],
 
   controllers: [
@@ -24,7 +29,11 @@ import { AuthService } from './auth.service';
 
   providers: [
     AuthService,
+    AuthGuard,
   ],
-
+  exports: [
+  AuthGuard,
+  JwtModule,
+],
 })
 export class AuthModule {}
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -3,10 +3,15 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './books.entits';
-
+import { AuthModule } from '../auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports:[TypeOrmModule.forFeature([Book])],
-  controllers: [BooksController],
+  imports: [
+  TypeOrmModule.forFeature([Book]),
+  AuthModule,
+  JwtModule,
+],
+controllers: [BooksController],
   providers: [BooksService],
 })
 export class BooksModule {}
